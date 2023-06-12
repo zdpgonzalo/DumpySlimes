c4d4ff05adbdc2fe29ca6e97c03cde9dca15b042
Ubermewtwo <86959459+Ubermewtwo@users.noreply.github.com>
2023-06-12 13:53:30 +0200

    Finalísimo

--- dumpySlimes/src/test/java/es/urjc/code/juegosenred/RestEjer1ConUiApplicationTests.java
+++ dumpySlimes/src/test/java/es/urjc/code/juegosenred/RestEjer1ConUiApplicationTests.java
@@ -1,12 +1,12 @@
 package es.urjc.code.juegosenred;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = RestEjer1ConUiApplication.class)
 public class RestEjer1ConUiApplicationTests {
 
 	@Test
